8c62f4ca28b7a4bda5a62c67ace7826e4651ca16
sakurouko <github@vecrel.com>
2026-01-16 11:48:38 +0800

    Update MainActivity with hardcoded palmServerEntrypoint and projectId for easier configuration, and upgrade palmid-native-sdk version to 1.6.8-veinshine for enhanced functionality.

--- app/src/main/java/com/palmid/sdk/example/MainActivity.kt
+++ app/src/main/java/com/palmid/sdk/example/MainActivity.kt
@@ -37,9 +37,9 @@ import com.palmid.sdk.example.ui.theme.PalmIDSDKExampleTheme
 private val TAG = "PalmIDSDKExample"
 
 class MainActivity : ComponentActivity() {
-    private var palmServerEntrypoint: String = ""
+    private var palmServerEntrypoint: String = "https://api2.palmid.com/saas"
     private var appServerEntrypoint: String = ""
-    private var projectId: String = ""  // Replace with your projectId
+    private var projectId: String = "6743918c6b44a78fd402bb43"  // Replace with your projectId
     private var requiredEnrollmentScans: Int = 2 // Optional. Required number of scans for enrollment. Default is 1.
 
     override fun onCreate(savedInstanceState: Bundle?) {
